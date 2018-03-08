33ae00adfa479dc9659a6f96a381232a22b4c162
Benz Le <33076057+Nova281@users.noreply.github.com>
2018-03-08 15:31:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ var context = canvas.getContext("2d");
 function drawImage(image, x, y, w, h) {
 	var imageObj = new Image();
 	imageObj.onload = function() {
-    	context.drawImage(imageObj, x, y, w, h);
+    		context.drawImage(imageObj, x, y, w, h);
   	}
   	imageObj.src = image;
 }
 
 // Game specific functions
 function makeBase() {
-  	// drawImage('alien.jpeg', 0, 0, 100, 100);
+  	drawImage('alien.jpeg', 0, 0, 100, 100);
 }
 
 // Event listeners
